25885391b04b2fa2157a2b1a526ff4827b8888b7
Ameur Saoudi <saoudiameur0@gmail.com>
2022-03-05 11:37:12 +0100

    updated page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-     
-      </header>
-    </div>
+    <Container id="main-container" className="d-grid h-100">
+      <Form id="sign-in-form" className="text-center p-3 w-100">
+        <img className="mb-4 bootstrap-logo" 
+              src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" 
+              alt="Bootstrap 5" />
+        <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
+        <Form.Group controlId="sign-in-email-address">
+          <Form.Control type="email" size="lg" placeholder="Email address" autoComplete="username" className="position-relative" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sign-in-password">
+          <Form.Control type="password" size="lg" placeholder="Password" autoComplete="current-password" className="position-relative" />
+        </Form.Group>
+        <Form.Group className="d-flex justify-content-center mb-4" controlId="remember-me">
+          <Form.Check label="Remember me" />
+        </Form.Group>
+        <div className="d-grid">
+          <Button variant="primary" onclick="myFunction()" size="lg">Sign in</Button>
+        </div>
+        <p className="mt-5 text-muted">&copy; 2021-2022</p>
+      </Form>
+      <script>
+       function myFunction() {
+        alert("SUCCESSFULLY SIGNED IN")}
+      </script>
+    </Container>
   );
 }
 
